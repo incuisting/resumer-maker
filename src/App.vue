bbd9a030d96fe162fe91e79f10a0a9f91d57ba3b
incuisting <xuyanting9634@163.com>
2017-07-19 23:37:57 +0800

    修改app.vue布局

--- src/App.vue
+++ src/App.vue
@@ -67,65 +67,66 @@ export default {
   height: 100vh;
   display: flex;
   flex-direction: column;
-}
 
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
 
-.topbar {
-  position: relative;
-  index: 1;
-  box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-}
-
-main {
-  display: flex;
-  flex: 1;
-  background: #ddd;
-  >.editor {
-    width: 40em;
-    margin: 16px 8px 16px 16px;
-    background: #fff;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-    border-radius: 4px;
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
     overflow: hidden;
   }
-  >.preview {
+
+  .topbar {
+    position: relative;
+    index: 1;
+    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+  }
+
+  main {
+    display: flex;
     flex: 1;
-    margin: 16px 16px 16px 8px;
-    background: #fff;
-    box-shadow: 0 0 3px rgba(0, 0, 0, .5);
-    border-radius: 4px;
-    overflow: hidden;
+    background: #ddd;
+    >.editor {
+      width: 40em;
+      margin: 16px 8px 16px 16px;
+      background: #fff;
+      box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+      border-radius: 4px;
+      overflow: hidden;
+    }
+    >.preview {
+      flex: 1;
+      margin: 16px 16px 16px 8px;
+      background: #fff;
+      box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+      border-radius: 4px;
+      overflow: hidden;
+    }
   }
-}
 
-.previewMode>#topbar {
-  display: none;
-}
+  .previewMode>#topbar {
+    display: none;
+  }
 
-.previewMode #editor {
-  display: none;
-}
+  .previewMode #editor {
+    display: none;
+  }
 
-.previewMode #preview {
-  max-width: 800px;
-  margin: 32px auto;
-}
+  .previewMode #preview {
+    max-width: 800px;
+    margin: 32px auto;
+  }
 
-#exitPreview {
-  display: none;
-}
+  #exitPreview {
+    display: none;
+  }
 
-.previewMode #exitPreview {
-  display: inline-block;
-  position: fixed;
-  right: 16px;
-  bottom: 16px;
+  .previewMode #exitPreview {
+    display: inline-block;
+    position: fixed;
+    right: 16px;
+    bottom: 16px;
+  }
 }
 </style>
