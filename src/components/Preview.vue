3962a72d87029535280147e623b99c9fa0d13768
incuisting <xuyanting9634@163.com>
2017-07-19 22:22:33 +0800

    对传入preview 的数据进行非空过滤，并渲染

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,15 +1,39 @@
 <template>
-    <div id="priview">
-        {{resume.profile.name}}
+    <div id="preview">
+        <h1>{{resume.profile.name || '请填写姓名'}}</h1>
+        <p>{{resume.profile.city || '请填写城市'}}</p>
+        <p>{{resume.profile.birth || '请填写生日'}}</p>
+         <section v-if="filter(resume.projects).length > 0">
+            
+            <h2>项目</h2>
+            <ol>
+                <li v-for="project in filter(resume.projects)" :key="project">
+                    {{project.name}}
+                    {{project.content}}
+                </li>
+            </ol>
+        </section> 
     </div>
 </template>
 <script>
 export default {
-  props:['resume']
+    props: ['resume'],
+    methods: {
+        filter(array) { //找出非空对象
+            return array.filter(item => !this.isEmpty(item))
+        },
+        isEmpty(object) { //只要有一个value不是false，就返回false
+            let empty = true
+            for (let key in object) {
+                if (object[key]) {
+                    empty = false
+                    break
+                }
+            }
+            return empty
+        }
+    }
+
 }
 </script>
 
-<style>
-#priview{
-}
-</style>
