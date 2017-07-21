e9ddda4dbff3eb474aea1c027aeef2f4ad957e81
incuisting <xuyanting9634@163.com>
2017-07-21 19:10:27 +0800

    完成组件的基础样式

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,63 @@
 <template>
-  
+  <div class=page>
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
 </template>
 
 <script>
-
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
 
 export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: '你好'
+    }
+  },
+  components: { Topbar, ResumeEditor, ResumePreview }
 }
 </script>
 
-<style lang="scss">
+<style>
+.page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #EAEBEC;
+}
 
+.page>main {
+  flex-grow: 1;
+}
+
+.page>main {
+  min-width: 1024px;
+  max-width: 1440px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: space-between;
+   width: 100%; 
+   padding:0 16px;
+  align-self: center;
+}
+
+#resumeEditor {
+  width: 35%;
+  background: #444;
+}
+
+#resumePreview {
+flex-grow: 1;
+margin-left: 16px;
+  background: #777;
+}
 </style>
