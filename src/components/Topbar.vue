7968e84032d34875ed41d621cec068ba38385ffe
incuisting <xuyanting9634@163.com>
2017-07-22 14:46:08 +0800

    添加注册的弹窗

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -3,16 +3,29 @@
         <div class="wrapper">
             <span class="logo">Resumer</span>
             <div class="actions">
-                <button class="primary">保存</button>
-                <button>预览</button>
+                <a class="button primary" href="#" @click.prevent="signUpDialogVisible =true">注册</a>
+                <MyDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
+                    我就是slot内容
+    
+                </MyDialog>
+                <a class="button" href="#">登录</a>
+                <button class="button primary">保存</button>
+                <button class="button">预览</button>
             </div>
         </div>
     </div>
 </template>
  
- <script>
+<script>
+import MyDialog from './MyDialog'
 export default {
-    name: 'Topbar'
+    name: 'Topbar',
+    data() {
+        return {
+            signUpDialogVisible: false
+        }
+    },
+    components: { MyDialog }
 }
 </script>
  
@@ -39,7 +52,7 @@ export default {
     }
 }
 
-button {
+.button {
     // 由于加了 scoped， 所以这个 button 选择器只在本组件内有效，不会影响其他组件
     width: 72px;
     height: 32px;
@@ -48,6 +61,11 @@ button {
     font-size: 18px; // 设计稿上是 20px，看起来太大，就改成 18px 了
     background: #ddd;
     color: #222;
+    text-decoration: none;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover {
         box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
     }
