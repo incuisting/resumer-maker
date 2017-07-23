515e5cd2e6a05fe33f608aa61502de3dc3d9997e
incuisting <xuyanting9634@163.com>
2017-07-23 10:06:28 +0800

    绝对绝对绝对不能让 data 中的对象的任何一个属性值变为 undefined 或者 null

--- src/lib/getAVUser.js
+++ src/lib/getAVUser.js
@@ -3,5 +3,8 @@ import AV from '../lib/leancloud'
 export default function(user) { //传参有问题，不懂
     var { id, attributes: { username } } = user || AV.User.current() || { attributes: {} }
         //https://leancloud.cn/docs/leanstorage_guide-js.html#当前用户 leancloud文档
-    return { id, username }
+    return {
+        id: id || '',
+        username: username || ''
+    }
 }
