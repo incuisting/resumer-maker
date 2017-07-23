1cfe833958d1294b11cf25c59d11bc392699233e
incuisting <xuyanting9634@163.com>
2017-07-23 14:57:39 +0800

    加入添加按钮，显示编辑区项目的中文标题

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -11,110 +11,118 @@
     </nav>
     <ol class="panels">
       <li v-for="item in resumeConfig" v-show="item.field === selected">
+        <h2 class="item-title">{{item.template.title}}</h2>
         <div v-if="item.type === 'array'">
           <div class="subitem" v-for="(subitem, i) in resume[item.field]">
             <div class="resumeField" v-for="(value,key) in subitem">
-              <label> {{key}} </label>
+              <label> {{item.template[key]}} </label>
               <input type="text" :value="value" @input="changeResumeField(`${item.field}.${i}.${key}`, $event.target.value)">
             </div>
             <hr>
           </div>
         </div>
         <div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
-          <label> {{key}} </label>
+          <label> {{item.template[key]}} </label>
           <input type="text" :value="value" @input="changeResumeField(`${item.field}.${key}`, $event.target.value)">
         </div>
+        <el-button type="success">添加</el-button>
       </li>
     </ol>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'ResumeEditor',
-    computed: {
-      selected:{
-        get(){
-          return this.$store.state.selected
-        },
-        set(value){
-          return this.$store.commit('switchTab', value)
-        }
+export default {
+  name: 'ResumeEditor',
+  computed: {
+    selected: {
+      get() {
+        return this.$store.state.selected
       },
-      resume (){
-        return this.$store.state.resume
-      },
-      resumeConfig(){
-        return this.$store.state.resumeConfig
+      set(value) {
+        return this.$store.commit('switchTab', value)
       }
     },
-    methods: {
-      changeResumeField(path, value){
-        this.$store.commit('updateResume',{
-          path,
-          value
-        })
-      }
+    resume() {
+      return this.$store.state.resume
+    },
+    resumeConfig() {
+      return this.$store.state.resumeConfig
+    }
+  },
+  methods: {
+    changeResumeField(path, value) {
+      this.$store.commit('updateResume', {
+        path,
+        value
+      })
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  #resumeEditor{
-    background:#ffffff;
-    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-    display: flex;
-    flex-direction: row;
-    overflow: auto;
-    > nav{
-      width: 80px;
-      background: black;
-      color: white;
-      > ol {
-        > li{
-          height: 48px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin-top: 16px;
-          margin-bottom: 16px;
-          &.active{
-            background: white;
-            color: black;
-          }
+#resumeEditor {
+  background: #ffffff;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: row;
+  overflow: auto;
+  >nav {
+    width: 80px;
+    background: black;
+    color: white;
+    >ol {
+      >li {
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 16px;
+        margin-bottom: 16px;
+        &.active {
+          background: white;
+          color: black;
         }
       }
     }
-    > .panels{
-      flex-grow: 1;
-      > li {
-        padding: 24px;
+  }
+  >.panels {
+    flex-grow: 1;
+    >li {
+      padding: 24px;
+      .item-title {
+        margin-bottom: .5em;
       }
     }
-    svg.icon{
-      width: 24px; // 原设计稿 32px 不好看，改成 24px
-      height: 24px;
-    }
   }
-  ol{
-    list-style: none;
+  svg.icon {
+    width: 24px; // 原设计稿 32px 不好看，改成 24px
+    height: 24px;
   }
-  .resumeField{
-    > label{
-      display: block;
-    }
-    input[type=text]{
-      margin: 16px 0;
-      border: 1px solid #ddd;
-      box-shadow:inset 0 1px 3px 0 rgba(0,0,0,0.25);
-      width: 100%;
-      height: 40px;
-      padding: 0 8px;
-    }
+}
+
+ol {
+  list-style: none;
+}
+
+.resumeField {
+  >label {
+    display: block;
   }
-  hr{
-    border: none;
-    border-top: 1px solid #ddd;
-    margin: 24px 0;
+  input[type=text] {
+    margin: 16px 0;
+    border: 1px solid #ddd;
+    box-shadow: inset 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+    width: 100%;
+    height: 40px;
+    padding: 0 8px;
   }
+}
+
+hr {
+  border: none;
+  border-top: 1px solid #ddd;
+  margin: 24px 0;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -13,27 +13,84 @@ export default new Vuex.Store({
             id: '',
             username: ''
         },
-        resumeConfig: [
-            { field: 'profile', icon: 'id', keys: ['name', 'city', 'title', 'birthday'] },
-            { field: 'workHistory', icon: 'work', type: 'array', keys: ['company', 'content'] },
-            { field: 'education', icon: 'book', type: 'array', keys: ['school', 'content'] },
-            { field: 'projects', icon: 'heart', type: 'array', keys: ['name', 'content'] },
-            { field: 'awards', icon: 'cup', type: 'array', keys: ['name', 'content'] },
-            { field: 'contacts', icon: 'phone', type: 'array', keys: ['contact', 'content'] },
+        resumeConfig: [{
+                field: 'profile',
+                icon: 'id',
+                keys: ['name', 'city', 'title', 'birthday'],
+                template: {
+                    title: '个人信息',
+                    name: '姓名',
+                    city: '城市',
+                    job: '职称',
+                    birthday: '出生年月'
+                }
+            },
+            {
+                field: 'workHistory',
+                icon: 'work',
+                type: 'array',
+                keys: ['company', 'content'],
+                template: {
+                    title: '工作经历',
+                    company: '公司',
+                    content: '工作内容',
+                }
+            },
+            {
+                field: 'education',
+                icon: 'book',
+                type: 'array',
+                keys: ['school', 'content'],
+                template: {
+                    title: '教育经历',
+                    school: '学校名称',
+                    content: '时间',
+                }
+            },
+            {
+                field: 'projects',
+                icon: 'heart',
+                type: 'array',
+                keys: ['name', 'content'],
+                template: {
+                    title: '项目经历',
+                    name: '项目名称',
+                    content: '项目内容',
+                }
+            },
+            {
+                field: 'awards',
+                icon: 'cup',
+                type: 'array',
+                keys: ['name', 'content'],
+                template: {
+                    title: '获奖信息',
+                    name: '名称',
+                    content: '获奖内容',
+                }
+            },
+            {
+                field: 'contacts',
+                icon: 'phone',
+                type: 'array',
+                keys: ['contact', 'content'],
+                template: {
+                    title: '联系方式',
+                    contact: '途径',
+                    content: '内容',
+                }
+            },
 
         ],
         resume: {}
     },
     mutations: {
         initState(state, payload) {
-            console.log('state.resumeConfig', state.resumeConfig)
             state.resumeConfig.map((item) => {
-                console.log('item.type', item)
                 if (item.type === 'array') {
                     //state.resume[item.field] = [] // 这样写 Vue 无法监听属性变化
                     Vue.set(state.resume, item.field, [])
                 } else {
-                    console.log(11)
 
                     //state.resume[item.field] = {} // 这样写 Vue 无法监听属性变化
                     Vue.set(state.resume, item.field, {})
@@ -44,7 +101,6 @@ export default new Vuex.Store({
                 }
             })
             Object.assign(state, payload)
-            console.log('state.resume', state.resume)
         },
         switchTab(state, payload) {
             state.selected = payload //提交荷载
