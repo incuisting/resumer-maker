0c36712cc666fb22f76831012d17f2d873ba6e7f
incuisting <xuyanting9634@163.com>
2017-07-29 11:57:44 +0800

    预览部分加入意向岗位

--- src/components/ResumePreview.vue
+++ src/components/ResumePreview.vue
@@ -6,6 +6,7 @@
                     <h1>
                         {{resume.profile.name}}
                     </h1>
+                    <h2>意向岗位:</h2>
                     <h2>{{resume.profile.title}}</h2>
                     <p>
                         <small>现居住：{{resume.profile.city}}</small>
